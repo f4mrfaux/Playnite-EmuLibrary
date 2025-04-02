5b26d913048710f3fb20974e6c69368dbb6a87a4
f4mrfaux <33949344+f4mrfaux@users.noreply.github.com>
2025-04-01 20:50:15 -0400

    Update GogInstallerGameInfo.cs

--- EmuLibrary/RomTypes/GogInstaller/GogInstallerGameInfo.cs
+++ EmuLibrary/RomTypes/GogInstaller/GogInstallerGameInfo.cs
@@ -1,1 +1,32 @@
+using System;
+using System.IO;
+using Playnite.SDK.Models;
+using EmuLibrary.RomTypes;
 
+namespace EmuLibrary.RomTypes.GogInstaller
+{
+    public class GogInstallerGameInfo : ELGameInfo
+    {
+        public GogInstallerGameInfo() : base(RomType.GogInstaller)
+        {
+        }
+
+        public GogInstallerGameInfo(string name, string path) : base(RomType.GogInstaller)
+        {
+            Name = name;
+            Path = path;
+            RomExtension = Path.GetExtension(path);
+            LastModified = File.GetLastWriteTime(path);
+        }
+
+        public override string GetDisplayName()
+        {
+            return Name;
+        }
+
+        public override string GetGameImagePath()
+        {
+            return Path;
+        }
+    }
+}
